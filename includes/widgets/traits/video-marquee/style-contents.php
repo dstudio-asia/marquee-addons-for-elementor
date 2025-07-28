d5e191a340a6443896205189c2c3625df1dffa70
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-07-28 12:29:19 +0600

    FIX: Fix edge shadow of video marquee

--- includes/widgets/traits/video-marquee/style-contents.php
+++ includes/widgets/traits/video-marquee/style-contents.php
@@ -79,20 +79,6 @@ trait Videomarquee_Style_Contents {
 		);
 
 		$this->add_group_control(
-			Group_Control_Box_Shadow::get_type(),
-			[
-				'name' => 'deensimc_video_box_shadow',
-				'selector' => '{{WRAPPER}} .deensimc-marquee.deensimc-shadow.deensimc-marquee-horizontal::before, 
-				{{WRAPPER}} .deensimc-marquee.deensimc-shadow.deensimc-marquee-horizontal::after, 
-				{{WRAPPER}} .deensimc-marquee.deensimc-shadow.deensimc-marquee-vertical::before, 
-				{{WRAPPER}} .deensimc-marquee.deensimc-shadow.deensimc-marquee-vertical::after',
-				'condition' => [
-					'deensimc_show_shadow_switch' => 'yes',
-				],
-			]
-		);
-
-		$this->add_group_control(
 			Group_Control_Border::get_type(),
 			[
 				'name' => 'deensimc_video_box_border',
@@ -111,6 +97,56 @@ trait Videomarquee_Style_Contents {
 					'{{WRAPPER}} .deensimc-marquee-group .deensimc-video-item iframe' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 					'{{WRAPPER}} .deensimc-marquee-group .deensimc-video-item video' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 					'{{WRAPPER}} .deensimc-marquee-group .deensimc-video-placeholder img' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'deensimc_video_marquee_edge_shadow_color',
+			[
+				'label' => esc_html__('Edge Shadow Color',  'marquee-addons-for-elementor'),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .deensimc-marquee.deensimc-shadow' => '--edge-shadow-color: {{VALUE}};',
+				],
+				'condition' => [
+					'deensimc_video_marquee_show_edge_shadow_switch' => 'yes',
+				],
+			]
+		);
+
+		$this->add_control(
+			Group_Control_Box_Shadow::get_type(),
+			[
+				'label' => esc_html__('Edge Shadow Spread', 'marquee-addons-for-elementor'),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => ['px', 'em', 'rem'],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 200,
+						'step' => 1,
+					],
+					'em' => [
+						'min' => 0,
+						'max' => 10,
+						'step' => 0.1,
+					],
+					'rem' => [
+						'min' => 0,
+						'max' => 10,
+						'step' => 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 50,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .deensimc-marquee.deensimc-shadow' => '--edge-shadow-spread: {{SIZE}}{{UNIT}};',
+				],
+				'condition' => [
+					'deensimc_video_marquee_show_edge_shadow_switch' => 'yes',
 				],
 			]
 		);
