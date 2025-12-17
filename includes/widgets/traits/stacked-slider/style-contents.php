1e94f4a85aa39bd75da775702babc431c4e5c2f7
wdNaimur <wd.naimur@gmail.com>
2025-12-17 13:00:03 +0600

    ref: update trait names for stacked slider widget

--- includes/widgets/traits/stacked-slider/style-contents.php
+++ includes/widgets/traits/stacked-slider/style-contents.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 
@@ -9,14 +9,15 @@ use \Elementor\Controls_Manager;
 use \Elementor\Group_Control_Typography;
 
 
-trait Stackedslider_Style_Contents {
+trait Deensimc_Stackedslider_Style_Contents
+{
 
-    protected function style_contents() 
-    {
-        $this->start_controls_section(
+	protected function style_contents()
+	{
+		$this->start_controls_section(
 			'deensimc_content_style_section',
 			[
-				'label' => esc_html__( 'Contents', 'marquee-addons-for-elementor' ),
+				'label' => esc_html__('Contents', 'marquee-addons-for-elementor'),
 				'tab' => Controls_Manager::TAB_STYLE,
 			]
 		);
@@ -24,19 +25,19 @@ trait Stackedslider_Style_Contents {
 		$this->add_responsive_control(
 			'deensimc_box_alignment_control',
 			[
-				'label' => esc_html__( 'Alignment', 'marquee-addons-for-elementor' ),
+				'label' => esc_html__('Alignment', 'marquee-addons-for-elementor'),
 				'type' =>  Controls_Manager::CHOOSE,
 				'options' => [
 					'start' => [
-						'title' => esc_html__( 'Left', 'marquee-addons-for-elementor' ),
+						'title' => esc_html__('Left', 'marquee-addons-for-elementor'),
 						'icon' => 'eicon-text-align-left',
 					],
 					'center' => [
-						'title' => esc_html__( 'Center', 'marquee-addons-for-elementor' ),
+						'title' => esc_html__('Center', 'marquee-addons-for-elementor'),
 						'icon' => 'eicon-text-align-center',
 					],
 					'end' => [
-						'title' => esc_html__( 'Right', 'marquee-addons-for-elementor' ),
+						'title' => esc_html__('Right', 'marquee-addons-for-elementor'),
 						'icon' => 'eicon-text-align-right',
 					],
 				],
@@ -52,7 +53,7 @@ trait Stackedslider_Style_Contents {
 		$this->add_responsive_control(
 			'deensimc_content_padding',
 			[
-				'label' => esc_html__( 'Padding', 'marquee-addons-for-elementor' ),
+				'label' => esc_html__('Padding', 'marquee-addons-for-elementor'),
 				'type' =>  Controls_Manager::DIMENSIONS,
 				'default' => [
 					'top' => 30,
@@ -62,7 +63,7 @@ trait Stackedslider_Style_Contents {
 					'unit' => 'px',
 					'isLinked' => true,
 				],
-				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+				'size_units' => ['px', '%', 'em', 'rem', 'custom'],
 				'selectors' => [
 					'{{WRAPPER}} .deensimc-slider-content' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -76,7 +77,7 @@ trait Stackedslider_Style_Contents {
 		$this->add_control(
 			'deensimc_content_button_heading',
 			[
-				'label' => esc_html__( 'Button', 'marquee-addons-for-elementor' ),
+				'label' => esc_html__('Button', 'marquee-addons-for-elementor'),
 				'type' =>  Controls_Manager::HEADING,
 				'separator' => 'before',
 			]
@@ -84,19 +85,19 @@ trait Stackedslider_Style_Contents {
 
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
-		   [
-			   'name' => 'deensimc_content_button_typography',
-			   'selector' => '{{WRAPPER}} .deensimc-image-slider-main .deensimc-slider-content .deensimc-content-btn',
-			   'separator' => 'after'
-		   ]
-	   );
+			[
+				'name' => 'deensimc_content_button_typography',
+				'selector' => '{{WRAPPER}} .deensimc-image-slider-main .deensimc-slider-content .deensimc-content-btn',
+				'separator' => 'after'
+			]
+		);
 
 		$this->add_responsive_control(
 			'deensimc_btn_padding',
 			[
-				'label' => esc_html__( 'Padding', 'marquee-addons-for-elementor' ),
+				'label' => esc_html__('Padding', 'marquee-addons-for-elementor'),
 				'type' =>  Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+				'size_units' => ['px', '%', 'em', 'rem', 'custom'],
 				'selectors' => [
 					'{{WRAPPER}} .deensimc-content-btn' => 'Padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -106,5 +107,5 @@ trait Stackedslider_Style_Contents {
 		$this->style_button_controls();
 
 		$this->end_controls_section();
-    }
+	}
 }
