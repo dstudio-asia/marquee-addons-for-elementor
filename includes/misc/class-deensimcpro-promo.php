89a4a1319ede14f8fac42f375f20dea66bf9526e
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-12-18 13:04:13 +0600

    ref: dynamically load pro widgets for promotion
    - Remove hardcoded promotion widgets; fetch from manifest using Manifest_Loader.
    - Filter pro widgets excluding 'extensions' category for promotion.
    - Merge with any existing promotion widgets in config.

--- includes/misc/class-deensimcpro-promo.php
+++ includes/misc/class-deensimcpro-promo.php
@@ -2,9 +2,14 @@
 
 namespace Deensimc_Marquee\Misc;
 
+require_once DEENSIMC__DIR__ . '/traits/Manifest_Loader.php';
+
+use Deensimc_Marquee\Manifest_Loader;
+
 
 trait Deensimcpro_Promo
 {
+    use Manifest_Loader;
 
     public function localize_promotion_script()
     {
@@ -22,109 +27,24 @@ trait Deensimcpro_Promo
 
     public function promote_pro_elements($config)
     {
+        $existing_promotion_widgets = isset($config['promotionWidgets']) ? $config['promotionWidgets'] : [];
+
+        $all_widgets = self::get_manifest();
+
         $promotion_widgets = [];
 
-        if (isset($config['promotionWidgets'])) {
-            $promotion_widgets = $config['promotionWidgets'];
+        foreach ($all_widgets as $name => $widget) {
+            if (!empty($widget['is_pro']) && $widget['cat'] !== 'extensions') {
+                $promotion_widgets[] = [
+                    'name'       => $name,
+                    'title'      => $widget['title'],
+                    'icon'       => $widget['icon'],
+                    'categories' => '["marquee_addons_pro_promo"]',
+                ];
+            }
         }
 
-        $combine_array = array_merge(
-            $promotion_widgets,
-            [
-                [
-                    'name'       => 'deensimcpro-3d-grid-marquee',
-                    'title'      => __('3D Grid Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-3d-grid-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-card-list',
-                    'title'      => __('Card Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-card-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-circular-text-pro',
-                    'title'      => __('Circular Text Rotation', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-circular-text-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-image-accordion',
-                    'title'      => __('Image Accordion Pro', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-image-accordion-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-media-marquee',
-                    'title'      => __('Media Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-media-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-post-marquee',
-                    'title'      => __('Post Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-post-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro_product_category_marquee',
-                    'title'      => __('Product Category Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-product-cat-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro_product_marquee',
-                    'title'      => __('Product Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-product-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-sticky-cards',
-                    'title'      => __('Sticky Cards', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-sticky-cards-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimc-testimonial-pro',
-                    'title'      => __('Advanced Testimonial Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-testimonial-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimc-smooth-text-pro',
-                    'title'      => __('Advanced Text Marquee', 'marquee-addons-for-elementor'),
-                    'icon'       => 'deensimcpro-text-marquee-icon eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-smart-tabs',
-                    'title'      => __('Smart Tabs', 'marquee-addons-for-elementor'),
-                    'icon'       => 'eicon-tabs eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-animated-list',
-                    'title'      => __('Animated List', 'marquee-addons-for-elementor'),
-                    'icon'       => 'eicon-post-list eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-bento-grid',
-                    'title'      => __('Bento Grid', 'marquee-addons-for-elementor'),
-                    'icon'       => 'eicon-gallery-grid eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-                [
-                    'name'       => 'deensimcpro-text-reveal',
-                    'title'      => __('Text Reveal', 'marquee-addons-for-elementor'),
-                    'icon'       => 'eicon-animation-text eicon-deensimc-pro',
-                    'categories' => '["marquee_addons_pro_promo"]',
-                ],
-            ]
-        );
-
-        $config['promotionWidgets'] = $combine_array;
+        $config['promotionWidgets'] = array_merge($existing_promotion_widgets, $promotion_widgets);
 
         return $config;
     }
