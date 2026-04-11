8d6be56f7cd60d7119f42da1aea525d272ccd421
wdNaimur <wd.naimur@gmail.com>
2026-04-11 15:46:57 +0600

    chore: Update version to 3.9.38

--- base.php
+++ base.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) exit;
 final class Base
 {
     private static $_instance = null;
-    const VERSION = '3.9.37';
+    const VERSION = '3.9.38';
 
     public function __construct()
     {
